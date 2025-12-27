19abb55b4c1af14079b227a16337d0cb8f8d227f
Marko K. S. <marko.seppanen@gmail.com>
2025-12-27 23:00:33 +0100

    Fixes + Rewording of phrase to more clear

--- src/components/RandomHeadline.tsx
+++ src/components/RandomHeadline.tsx
@@ -1,7 +1,11 @@
+type Props = {
+    headlines: readonly string[]
+}
+
 /*
  * Random on each page load (client-side).
  */
-export function RandomHeadlineH1({ headlines }: { headlines: string[] }) {
+export function RandomHeadlineH1({ headlines }: Props) {
     const random = headlines[Math.floor(Math.random() * headlines.length)]
     return (
         <h1 className="text-3xl font-semibold tracking-tight md:text-5xl">
