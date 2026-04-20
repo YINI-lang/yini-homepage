3c51000fe795134642513fde06f56366c714d260
Marko K. S. <marko.seppanen@gmail.com>
2026-04-20 22:24:49 +0200

    Updated to use getDayOfMonth(..), easier track manually

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,6 +2,10 @@
  * Date-only utilities.
  */
 
+export function getDayOfMonth(date = new Date()): number {
+    return date.getDate() // Returns the day number within the month: 1 to 31.
+}
+
 export function getDayOfYear(date = new Date()): number {
     const startOfYear = new Date(date.getFullYear(), 0, 0)
     const diff = date.getTime() - startOfYear.getTime()
